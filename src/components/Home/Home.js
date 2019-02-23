1b249441e7b96cfc86c23a7033fac19f17768f8f
Erick Henriquez <henriquez93550@yahoo.com>
2019-02-23 12:33:21 -0600

    working on search bar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -30,6 +30,23 @@ class Home extends Component {
     this.fetchItems(endpoint);
   }
 
+  searchItems = searchTerm => {
+    console.log(searchTerm);
+    let endpoint = "";
+    this.setState({
+      movies: [],
+      loading: true,
+      searchTerm
+    });
+
+    if (searchTerm === "") {
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    } else {
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
+    }
+    this.fetchItems(endpoint);
+  };
+
   loadMoreItems = () => {
     let endpoint = "";
     this.setState({ loading: true });
@@ -38,7 +55,7 @@ class Home extends Component {
       endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this
         .state.currentPage + 1}`;
     } else {
-      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&query${
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query${
         this.state.searchTerm
       }&page=${this.state.currentPage + 1}`;
     }
@@ -49,17 +66,17 @@ class Home extends Component {
     fetch(endpoint)
       .then(result => result.json())
       .then(result => {
-        console.log(result);
         this.setState({
-          // results is from the API
           movies: [...this.state.movies, ...result.results],
           heroImage: this.state.heroImage || result.results[0],
           loading: false,
           currentPage: result.page,
           totalPages: result.total_pages
         });
-      });
+      })
+      .catch(error => console.error("Error:", error));
   };
+
   render() {
     return (
       <div className="rmdb-home">
@@ -72,7 +89,7 @@ class Home extends Component {
               title={this.state.heroImage.original_title}
               text={this.state.heroImage.overview}
             />
-            <SearchBar />
+            <SearchBar callback={this.searchItems} />
           </div>
         ) : null}
         <FourColGrid />
--- src/components/elements/SearchBar/SearchBar.js
+++ src/components/elements/SearchBar/SearchBar.js
@@ -3,9 +3,33 @@ import FontAwesome from "react-fontawesome";
 import "./SearchBar.css";
 
 class SearchBar extends Component {
-  state = {};
+  state = {
+    value: ""
+  };
+
+  doSearch = event => {
+    this.setState({ value: event.target.value });
+    clearTimeout(this.timeout);
+    // waits half a second before starting the search
+    this.timeout = setTimeout(() => {
+      this.props.callback(this.state.value);
+    }, 500);
+  };
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="rmdb-searchbar">
+        <div className="rmdb-searchbar-content">
+          <FontAwesome className="rmdb-fa-search" name="search" size="2x" />
+          <input
+            type="text"
+            className="rmdb-searchbar-input"
+            placeholder="Search"
+            onChange={this.doSearch}
+            value={this.state.value}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
